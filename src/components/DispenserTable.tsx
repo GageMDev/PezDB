84d52b7a82ba294c49d0808dac82750f409adb79
GageM <gage.merrick@gmail.com>
2023-12-25 09:13:30 -0500

    better handling of no image

--- src/components/DispenserTable.tsx
+++ src/components/DispenserTable.tsx
@@ -9,9 +9,13 @@ const fetcher = (url: string | URL | Request) => fetch(url).then(res => res.json
 const columns: GridColDef[] = [
     {field: 'name', headerName: 'Name', flex: 1 },
     {
-        field: 'image', headerName: 'Image', minWidth: 250, flex: 1, renderCell: (params: GridRenderCellParams<any, Date>) => (
-            <Image src={`/api/photos${params.value}`} width={150} height={150} alt={`photo`}/>
-        ),
+        field: 'image', headerName: 'Image', minWidth: 250, flex: 1, renderCell: (params: GridRenderCellParams<any, Date>) => {
+            return params.value ? (
+                <Image src={`/api/photos${params.value}`} width={150} height={150} alt={`photo`} />
+            ) : (
+                <></>
+            );
+        },
     },
     {field: 'stemColor', headerName: 'Stem Color', flex: 1},
 ];
